0235e2c8b4c87a2b3e8919c2ab89ab5f65c98c3c
Francis Gallagher <gallfrancis@live.ie>
2015-10-21 12:42:02 +0100

    Added styling, clean urls and partial view

--- SportsStore.Domain/Concrete/EFDbContext.cs
+++ SportsStore.Domain/Concrete/EFDbContext.cs
@@ -18,7 +18,7 @@ namespace SportsStore.Domain.Concrete
         }
     }
 
-    public class dbInitializer : DropCreateDatabaseIfModelChanges<EFDbContext>
+    public class dbInitializer : DropCreateDatabaseAlways<EFDbContext>
     {
         protected override void Seed(EFDbContext context)
         {
@@ -28,7 +28,14 @@ namespace SportsStore.Domain.Concrete
                 new Product(){Name="Gaelic ball",Description="Offical World Cup Ball",Price=19.99m,Catergory="Soccer"}, 
                 new Product(){Name="Basket ball",Description="Offical World Cup Ball",Price=19.99m,Catergory="Soccer"}, 
                 new Product(){Name="Cue ball",Description="Offical World Cup Ball",Price=19.99m,Catergory="Soccer"}, 
-                new Product(){Name="Champions league ball",Description="Offical World Cup Ball",Price=19.99m,Catergory="Soccer"} 
+                new Product(){Name="Soccer hat",Description="Offical World Cup Ball",Price=19.99m,Catergory="Soccer"}, 
+                new Product(){Name="Corner flags",Description="Offical World Cup Ball",Price=19.99m,Catergory="Soccer"}, 
+                new Product(){Name="Board game",Description="Offical World Cup Ball",Price=19.99m,Catergory="Soccer"}, 
+                new Product(){Name="Cones",Description="Offical World Cup Ball",Price=19.99m,Catergory="Soccer"}, 
+                new Product(){Name="Gloves",Description="Offical World Cup Ball",Price=19.99m,Catergory="Soccer"}, 
+                new Product(){Name="Whistles",Description="Offical World Cup Ball",Price=19.99m,Catergory="Soccer"}, 
+                new Product(){Name="Liverpool Jersey",Description="Offical World Cup Ball",Price=19.99m,Catergory="Soccer"},
+                new Product(){Name="Football boots",Description="Offical World Cup Ball",Price=19.99m,Catergory="Soccer"} 
             };
 
             foreach(Product p in products)
