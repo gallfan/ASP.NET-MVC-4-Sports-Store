4a2241da08cc8c7a087ba866b93580508af4a20f
Francis Gallagher <gallfrancis@live.ie>
2015-10-23 13:06:22 +0100

    Cart func half done

---
+++ SportsStore.Domain/Entities/Cart.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SportsStore.Domain.Entities
+{
+   public class Cart
+    {
+       private List<CartLine> LineCollection = new List<CartLine>();
+
+       public void AddItem(Product product, int quantity)
+       {
+           CartLine line = LineCollection
+                           .Where(p =>p.Product.ProductID == product.ProductID)
+                           .FirstOrDefault();
+           if (line == null)
+           {
+               LineCollection.Add(new CartLine{Product = product,
+               Quantity = quantity});
+           }
+           else
+           {
+               line.Quantity += quantity;
+           }
+       }
+
+       public void RemoveLine(Product product)
+       {
+           LineCollection.RemoveAll(l => l.Product.ProductID == product.ProductID);
+       }
+
+       public decimal ComputeTotalValue()
+       {
+           return LineCollection.Sum(e => e.Product.Price * e.Quantity);
+       }
+
+       public void Clear()
+       {
+           LineCollection.Clear();
+       }
+
+       public IEnumerable<CartLine> Lines
+       {
+           get { return LineCollection; }
+       }
+    }
+
+   public class CartLine 
+   {
+       public Product Product { get; set; }
+       public int Quantity { get; set; }
+   }
+}
